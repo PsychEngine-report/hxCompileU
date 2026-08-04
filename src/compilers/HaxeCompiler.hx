bc4f98ee7c276cee8b0b7c21a670a6c972e7d070
Ethantobot <ethantobot7@gmail.com>
2026-08-04 03:13:40 +0200

    Update HaxeCompiler.hx

--- src/compilers/HaxeCompiler.hx
+++ src/compilers/HaxeCompiler.hx
@@ -74,6 +74,7 @@ class HaxeCompiler {
 	-main ${jsonFile.haxeConfig.hxMain}
 	-D message.reporting=${reportStyle}
 	# Default and required libraries #########
+	-lib reflaxe
 	-lib reflaxe.cpp
 	-lib hxu_wut
 	# Reflaxe/C++ parameters #########
